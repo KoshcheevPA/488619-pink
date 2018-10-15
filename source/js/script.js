6b4da96b8df68f0704f9d21bd46b67a6b146b05c
KoshcheevPA <pavelkoshcheev@yandex.ru>
2018-10-15 21:18:17 +0500

    Реализовано закрытие/открытие меню

---
+++ source/js/script.js
@@ -0,0 +1,34 @@
+var navMain = document.querySelector('.main-navigation');
+var navToggle = document.querySelector('.main-navigation__toggle');
+var siteList = document.querySelector('.site-list');
+var headerColor = document.querySelector('.page-header');
+
+navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('main-navigation__toggle--nojs')
+siteList.classList.remove('site-list--nojs');
+headerColor.classList.remove('page-header--nojs');;
+
+navToggle.addEventListener('click', function() {
+  if (siteList.classList.contains('site-list--closed')) {
+    siteList.classList.remove('site-list--closed');
+    siteList.classList.add('site-list--opened');
+
+    navToggle.classList.remove('main-navigation__toggle--opened');
+    navToggle.classList.add('main-navigation__toggle--closed');
+
+    headerColor.classList.remove('page-header--menu-closed');
+    headerColor.classList.add('page-header--menu-opened');
+    // siteList.classList.remove('main-navigation__toggle--close');
+    // siteList.classList.add('main-navigation__toggle--open');
+
+  } else {
+    siteList.classList.add('site-list--closed');
+    siteList.classList.remove('site-list--opened');
+
+    navToggle.classList.add('main-navigation__toggle--opened');
+    navToggle.classList.remove('main-navigation__toggle--closed');
+
+    headerColor.classList.add('page-header--menu-closed');
+    headerColor.classList.remove('page-header--menu-opened');
+  }
+});
