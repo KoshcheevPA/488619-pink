cb15619fc8b1cc79b1a09a8e39212f765637e879
KoshcheevPA <pavelkoshcheev@yandex.ru>
2018-10-26 05:56:01 +0500

    Фикс порядка свойст css. Реализация появления попапов

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,16 @@ var cross =  document.querySelector('.main-navigation__image--cross');
 
 var map = document.querySelector('.contacts__map');
 
+var emailUser = document.querySelector('[name=email]');
+var nameUser = document.querySelector('[name=name]');
+var surnameUser = document.querySelector('[name=surname]');
+var formFields = document.querySelector('.form-field');
+var formButton = document.querySelector('.form-field__submit-button');
+var popupFailMessage = document.querySelector('.submit-unsucsess');
+var popupMessage = document.querySelector('.submit-sucsess');
+var popupButtonSucsess = document.querySelector('.submit-sucsess__close-button');
+var popupButtonUnsucsess = document.querySelector('.submit-unsucsess__close-button');
+
 navMain.classList.remove('main-nav--nojs');
 navToggle.classList.remove('main-navigation__toggle--nojs');
 siteList.classList.remove('site-list--nojs');
@@ -41,6 +51,28 @@ navToggle.addEventListener('click', function() {
     cross.classList.remove('main-navigation__image--opened');
     burger.classList.add('main-navigation__image--opened');
   }
+});
+
+
+formButton.addEventListener('click', function (evt) {
+  if (!emailUser.value || !nameUser.value || !surnameUser.value) {
+    popupMessage.classList.remove('submit-sucsess--opened');
+    popupFailMessage.classList.remove('submit-unsucsess--opened');
+    popupFailMessage.classList.add('submit-unsucsess--opened');
+
+  } else {
+    popupFailMessage.classList.remove('submit-unsucsess--opened');
+    popupMessage.classList.remove('submit-sucsess--opened');
+    popupMessage.classList.add('submit-sucsess--opened');
+  }
+});
+
+popupButtonSucsess.addEventListener('click', function (evt) {
+  popupMessage.classList.remove('submit-sucsess--opened');
+});
+
+popupButtonUnsucsess.addEventListener('click', function (evt) {
+  popupFailMessage.classList.remove('submit-unsucsess--opened');
 });
 
 
